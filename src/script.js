9b91203100859458873ea14a8769b7e811548763
Luis Yan Pederico <luisyanpederico@gmail.com>
2023-04-23 18:27:45 +0300

    create

---
+++ src/script.js
@@ -0,0 +1,9 @@
+import ethers from 'ethers';
+const provider = ethers.getDefaultProvider();
+
+const main = async () => {
+    const balance = await provider.getBalance(`vitalic.eth`);
+    console.log(`ETH Balance of vitalik: ${ethers.utils.formatEther(balance)}ETH`);
+}
+
+main()
